3849851d4f4984416160b8681f2c8493c893f6f6
Willian Agostini <willian.agostini@gmail.com>
2023-12-16 23:37:18 -0300

    rm SpeedInsights

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Chat from "@/components/Chat";
 import MobileSiderbar from "@/components/MobileSidebar";
 import Sidebar from "@/components/Sidebar";
